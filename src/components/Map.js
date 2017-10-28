470d0238324bc0398458018b12a8da97493499ac
djstojadin <djstojadin@gmail.com>
2017-10-28 23:21:32 +0200

    add google map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,30 @@
 import React from "react";
-
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 import style from "./Map.css";
 
-export default function Map() {
-  return (
-    <div className={style.Map}>
-      map <comment />
-    </div>
-  );
-}
+const Map = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: 44.662628, lng: 20.8558253 }}
+    >
+      {props.isMarkerShown && (
+        <div>
+          <Marker
+            position={{ lat: 44.666060696242575, lng: 20.922874570200054 }}
+          />
+          <Marker
+            position={{ lat: 44.665924901964274, lng: 20.923085525626245 }}
+          />
+        </div>
+      )}
+    </GoogleMap>
+  ))
+);
+
+export default Map;
--- src/containers/App.js
+++ src/containers/App.js
@@ -34,7 +34,13 @@ export default class App extends Component {
         <Header />
         <div className={style.AppContent}>
           <Listing />
-          <Map />
+          <Map
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
         </div>
       </div>
     );
