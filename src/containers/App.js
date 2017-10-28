85bf430851b0b4b977616c0800fb18747e97d737
djstojadin <djstojadin@gmail.com>
2017-10-28 23:06:56 +0200

    setting up and testing foursquare API

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,27 @@ import Map from "../components/Map";
 
 import style from "./App.css";
 
+const foursquare = require("react-foursquare")({
+  clientID: "Q0203EYLNNSJORXIRDT40X1UKPYTDI0CQ0MB0VT1WCFYT0Z3",
+  clientSecret: "LZBHTB3GPV14Q15UV3PFBERVNHIBRWYCNZABEWGGUKAQKBQQ"
+});
+
+const params = {
+  ll: "44.662628,20.8558253",
+  categories: "coffee"
+};
+
 export default class App extends Component {
+  state = {
+    items: []
+  };
+
+  componentDidMount = () => {
+    foursquare.venues.getVenues(params).then(res => {
+      console.log(res);
+    });
+  };
+
   render() {
     return (
       <div className={style.App}>
