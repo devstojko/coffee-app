fe31f610d3c995354f598fb36326ff4c6a41149b
djstojadin <djstojadin@gmail.com>
2017-10-31 21:42:56 +0100

    cant find or closed coffee shop message

--- src/components/MapWithMarker.js
+++ src/components/MapWithMarker.js
@@ -12,7 +12,7 @@ const MapWithMarker = compose(
   withRouter,
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyD20VEsdOtfZmtG30il5O2KTYPQk_SMNwc&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: "100%" }} />,
     containerElement: <div style={{ height: "100%" }} />,
     mapElement: <div style={{ height: "100%" }} />
